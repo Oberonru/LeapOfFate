403204b70fec00c3ed090d05ceaa3b5e003d521f
Alexey <Oberonru@mail.ru>
2025-06-10 15:21:27 +0600

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,48 +1,48 @@
 using UnityEngine;
 
+//[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float gravity = -9.8f;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float jumpHeight = 2f;
-    [SerializeField] private CharacterController controller;
-    [SerializeField] private Transform playerTransform;
-    [SerializeField] private Transform cameraTransform;
-    [SerializeField] private float cameraSpeed = 1;
-    [SerializeField] private float offsetY = 1.5f;
-    [SerializeField] private GameObject stopPanel;
-    private bool isGrounded;
+
+    private CharacterController controller;
     private Vector3 velocity;
+    private bool isGrounded;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
-        if (!stopPanel.activeSelf)
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
         {
-            isGrounded = controller.isGrounded;
-            Vector3 target = new Vector3(playerTransform.position.x, playerTransform.position.y + offsetY, cameraTransform.position.z);
-            cameraTransform.position = Vector3.Lerp(cameraTransform.position, target, cameraSpeed * Time.deltaTime);
-            float move = Input.GetAxis("Horizontal");
-            if (move > 0)
-            {
-                playerTransform.rotation = Quaternion.Euler(0, 90, 0);
-            }
-            else if (move < 0) 
-            {
-                playerTransform.rotation = Quaternion.Euler(0, -90, 0);
-            }
-                    
-            if (isGrounded && velocity.y < 0)
-            {
-                velocity.y = -2f;
-            }
-            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            {
-                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            }
-            controller.Move(speed * Time.deltaTime * Vector3.right * Input.GetAxis("Horizontal"));
-            velocity.y += gravity * Time.deltaTime;
-            controller.Move(velocity * Time.deltaTime);
+            velocity.y = -2f;
         }
-        
+
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(moveHor, 0, moveVer);
+
+        // Поворот в сторону движения, если есть движение
+        if (move.sqrMagnitude > 0.01f)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.deltaTime);
+        }
+
+        controller.Move(move.normalized * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
